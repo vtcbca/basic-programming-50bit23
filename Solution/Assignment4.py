9ece6f44f0ac80423a7e45202973dd5850a9a8e7
50bit23 <50bit23@vtcbcsr.edu.in>
2024-12-24 11:15:10 +0530

    Create Assignment4.py

---
+++ Solution/Assignment4.py
@@ -0,0 +1,6 @@
+def reverse_string_reversed(s):
+    return ''.join(reversed(s))
+
+# Example usage
+input_string = input("Enter a string: ")
+print(f"Reversed string: {reverse_string_reversed(input_string)}")
